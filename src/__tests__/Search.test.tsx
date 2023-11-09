f00dd22eb4d45fdff11dc0bfd1aa74f560e4f7ab
jaypoojara <jay.poojara@gmail.com>
2023-11-09 13:49:18 +0530

    added search test case

---
+++ src/__tests__/Search.test.tsx
@@ -0,0 +1,74 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
+import SearchPage from "../pages/Search";
+import { ThemeProvider } from "styled-components";
+import { useQuery } from "@apollo/client";
+import { theme } from "../common/theme";
+import mockData from '../mocks/mockData.json';
+import searchedMockData from '../mocks/searchedMockData.json';
+jest.mock("@apollo/client");
+
+describe("SearchPage", () => {
+  it("should match snapshot", async () => {
+    (useQuery as jest.Mock).mockReturnValueOnce({
+      loading: false,
+      error: null,
+      data: mockData,
+    });
+    const { container } = render(
+      <MockedProvider>
+        <ThemeProvider theme={theme}>
+          <SearchPage />
+        </ThemeProvider>
+      </MockedProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render content card data", async () => {
+    (useQuery as jest.Mock).mockReturnValueOnce({
+      loading: false,
+      error: null,
+      data: mockData,
+    });
+    render(
+      <MockedProvider>
+        <ThemeProvider theme={theme}>
+          <SearchPage />
+        </ThemeProvider>
+      </MockedProvider>
+    );
+    const defaultName = "test qa";
+    expect(screen.getByText(defaultName)).toBeInTheDocument();
+
+  });
+
+  it("should render searched content card data", async () => {
+    (useQuery as jest.Mock).mockReturnValueOnce({
+      loading: false,
+      error: null,
+      data: mockData,
+    });
+    render(
+      <MockedProvider>
+        <ThemeProvider theme={theme}>
+          <SearchPage />
+        </ThemeProvider>
+      </MockedProvider>
+    );
+    const defaultName = "test developer";
+    const searchTerm = "test qa";
+    expect(screen.getByText(searchTerm)).toBeInTheDocument();
+
+    const searchInput = screen.getByTestId("searchInput");
+    
+    (useQuery as jest.Mock).mockReturnValueOnce({
+      loading: false,
+      error: null,
+      data: searchedMockData,
+    });
+    fireEvent.change(searchInput, { target: { value: "test developers" } });
+    fireEvent.keyDown(searchInput, { key: "Enter", code: "Enter" });
+    expect(screen.getByText(defaultName)).toBeInTheDocument();
+  });
+});
